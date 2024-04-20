70c4fe5344537e90db5eea60e70511a05734046a
fcjcbk <1097086201@qq.com>
2024-04-20 10:58:13 +0800

    remove forget password button

--- src/views/login/components/login-form.vue
+++ src/views/login/components/login-form.vue
@@ -50,19 +50,19 @@
           >
             {{ $t('login.form.rememberPassword') }}
           </a-checkbox>
-          <a-link>{{ $t('login.form.forgetPassword') }}</a-link>
+          <a-link>Contact us</a-link>
         </div>
         <a-button type="primary" html-type="submit" long :loading="loading">
           {{ $t('login.form.login') }}
         </a-button>
-        <a-button
+        <!-- <a-button
           type="text"
           long
           class="login-form-register-btn"
           @click="redirectToRegister"
         >
           {{ $t('login.form.register') }}
-        </a-button>
+        </a-button> -->
       </a-space>
     </a-form>
   </div>
