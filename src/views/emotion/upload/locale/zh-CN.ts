ab5b83d251b57abfabeb3c53db8ebf29e82032cf
fcjcbk <1097086201@qq.com>
2024-05-04 10:01:41 +0800

    feat: 优化上传照片界面

---
+++ src/views/emotion/upload/locale/zh-CN.ts
@@ -0,0 +1,8 @@
+export default {
+  'menu.detect': '情绪检测',
+  'menu.detect.upload': '上传照片',
+  'emotion.detect.upload.title': '上传照片',
+  'emotion.detect.upload.success.title': '上传照片成功',
+  'emotion.detect.upload.success.subTitle': '情绪检测报告生成成功',
+  'emotion.detect.upload.view': '查看报告',
+};
