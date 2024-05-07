d3d2eb8d42e9b68dc77b30e8db8fe23d167a027f
笑九天IX <1694528260@qq.com>
2024-05-07 12:36:09 +0800

    制作情绪报告卡片

--- src/views/emotion/components/emotionCard.vue
+++ src/views/emotion/components/emotionCard.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <a-card :style="{ width: '360px' }">
     <template #actions>
@@ -14,22 +10,29 @@
           overflow: 'hidden',
         }"
       >
-        <img
-            :style="{ width: '50%',height: '80%', transform: 'translate(50%,20%)' }"
-            alt="dessert"
-            src="https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a20012a2d4d5b9db43dfc6a01fe508c0.png~tplv-uwbnlip3yd-webp.webp"
-        />
+<!--        <img-->
+<!--            :style="{ width: '50%',height: '80%', transform: 'translate(50%,15%)' }"-->
+<!--            alt="dessert"-->
+<!--            src="https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a20012a2d4d5b9db43dfc6a01fe508c0.png~tplv-uwbnlip3yd-webp.webp"-->
+<!--        />-->
+        <Chart style="width: 100%; height: 300px" :option="chartOption" />
       </div>
     </template>
-    <a-card-meta title="Card Title" description="This is the description">
+    <a-card-meta title="2024年5月7日">
+      <template #description>
+        愤怒 87.5%<br>
+        <br>
+        <b>可能会导致：</b> <br>
+        极端和危险的行为，如严重的暴力或破坏行为，需要立即的专业干预。<br>
+      </template>
       <template #avatar>
         <div
             :style="{ display: 'flex', alignItems: 'center', color: '#1D2129' }"
         >
-          <a-avatar :size="24" :style="{ marginRight: '8px' }">
+          <a-avatar :size="24" :style="{ marginRight: '8px'}">
             A
           </a-avatar>
-          <a-typography-text>Username</a-typography-text>
+          <a-typography-text style="user-select: none">Username</a-typography-text>
         </div>
       </template>
     </a-card-meta>
@@ -37,25 +40,39 @@
 </template>
 
 <script>
-import {
-  IconMore,
-} from '@arco-design/web-vue/es/icon';
+  import {
+    IconMore,
+  } from '@arco-design/web-vue/es/icon';
+  import useChartOption from '@/hooks/chart-option';
+  // Todo: 仪表盘
+  const { chartOption } = useChartOption((isDark) => {
+    const graphicElementStyle = {
+      textAlign: 'center',
+      fill: isDark ? 'rgba(255,255,255,0.7)' : '#4E5969',
+      fontSize: 14,
+      lineWidth: 10,
+      fontWeight: 'bold',
+    };
+    return {
 
-export default {
-  components: { IconMore },
-};
+  };
+});
+
+  export default {
+    components: { IconMore },
+  };
 </script>
 <style scoped>
-.icon-hover {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 24px;
-  height: 24px;
-  border-radius: 50%;
-  transition: all 0.1s;
-}
-.icon-hover:hover {
-  background-color: rgb(var(--gray-2));
-}
+  .icon-hover {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    transition: all 0.1s;
+  }
+  .icon-hover:hover {
+    background-color: rgb(var(--gray-2));
+  }
 </style>
