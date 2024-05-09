7e78bc4a6cacd1c68885793249de9e94155516e4
笑九天IX <1694528260@qq.com>
2024-05-09 11:14:54 +0800

    Todo: upload.ts 的 CORS

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -2,12 +2,18 @@ import axios from 'axios';
 import { LoginData, LoginRes } from '@/api/user';
 
 const instance = axios.create({
-  // Todo: 联系后端人员，确认需要请求的IP地址和端口
-  baseURL: 'http://8.134.248.200:5000',
+  // Todo: CORS问题
+  // header的authorization是临时的
+  baseURL: 'http://47.120.44.17:8080',
+  headers: {
+    Authorization:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbXBsb3llZUlkIjp7ImVtcGxveWVlSWQiOiI2IiwiZW1wbG95ZWVSb2xlIjoxfSwiZW1wbG95ZWVSb2xlIjp7ImVtcGxveWVlSWQiOiI2IiwiZW1wbG95ZWVSb2xlIjoxfSwiZXhwIjoxNzE1MjI2OTc3fQ.dQbQ91KFSY5QGDAbWWiQ1CMpKxE1BE_nC2XhJ-nIGMc',
+    Accept: '*/*',
+  },
 });
 
 export function uploadPhoto(photo: File) {
-  return instance.post<File>('/analyze-emotion', photo);
+  return instance.post<File>('/emotion/detect', photo);
 }
 
 export function login(data: LoginData) {
