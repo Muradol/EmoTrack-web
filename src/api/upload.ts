082f92e4368f679110d819257d480bc743679daf
笑九天IX <1694528260@qq.com>
2024-05-11 16:03:30 +0800

    微调

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -11,10 +11,10 @@ export interface emotionData{
 export function uploadPhoto(photo: File) {
   const formData = new FormData();
   formData.append('file', photo);
-  return axios.post<File>('/api/emotion/detect', formData, {
+  return axios.post<emotionData>('/api/emotion/detect', formData, {
     headers: {
       Authorization:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbXBsb3llZUlkIjp7ImVtcGxveWVlSWQiOiI2IiwiZW1wbG95ZWVSb2xlIjoxfSwiZW1wbG95ZWVSb2xlIjp7ImVtcGxveWVlSWQiOiI2IiwiZW1wbG95ZWVSb2xlIjoxfSwiZXhwIjoxNzE1NDEzMjg4fQ._HGjo1wDqotRlUrI1yKaWKcSuLbCoLIhLzC2HcjTV4I',
+      '',
       Accept: '*/*',
     },
   });
